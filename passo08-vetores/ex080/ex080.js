5752f0f60ed8a050c586246e34d0c4f415fa3006
cledisoncode <cledison.viana.ti@gmail.com>
2025-03-01 00:57:54 -0300

    atualizacao - 01/03/2025
    criacao e resolucao do exercicio 80

---
+++ passo08-vetores/ex080/ex080.js
@@ -0,0 +1,30 @@
+let vetorNumerosSorteados = [];
+    for(let i = 0; i < 30; i++) {
+        let numerosSorteados = parseInt(Math.random() * 15 + 1);
+        vetorNumerosSorteados.push(numerosSorteados);
+    }
+    console.log(vetorNumerosSorteados);
+
+function verificar() {
+    let num =  document.getElementById('fnum');
+    let vetorNumeroEncontrado = [];
+    let posicaoNumeroEncontrado = [];
+
+    for(let i = 0; i < vetorNumerosSorteados.length;i++){
+        if(vetorNumerosSorteados[i] === Number(num.value)) {
+            vetorNumeroEncontrado.push(vetorNumerosSorteados[i]);
+            posicaoNumeroEncontrado.push(i);
+        } 
+    }
+    console.log(vetorNumeroEncontrado);
+    
+    let palavraPosicao = vetorNumeroEncontrado.length > 1? 'nos índices' : 'no índice';
+    if(vetorNumeroEncontrado.length > 0){
+    alert(`Os números sorteados foram [${vetorNumerosSorteados}].
+        O número ${num.value} foi encontrado ${vetorNumeroEncontrado.length}x, ${palavraPosicao} (${posicaoNumeroEncontrado}).
+        `);
+
+    } else {
+        alert(`O número digitado não foi encontrado!.Tente outro número`);
+    }
+}
