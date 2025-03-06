d3d66c80d908c383fbc6519f2144340368866371
cledisoncode <cledison.viana.ti@gmail.com>
2025-03-06 02:38:18 -0300

    atualizacao - 06/03/25
    resolucao do exercicio 81

--- passo08-vetores/ex081/ex081.js
+++ passo08-vetores/ex081/ex081.js
@@ -4,10 +4,10 @@
         let somaIdades = 0;
         let mediaIdades = 0;
         let posicaoPessoasMais25 = [];
-        let maiorIdade = [];
+        let maiorIdade = 0;
         let posicaoMaiorIdade = [];
         
-            for(let i = 0; i < 3; i++) {
+            for(let i = 0; i < 8; i++) {
                 let idades = Number(prompt(`Insira a idade da pessoa ${contador++}`));
                 vetorIdades.push(idades);
 
@@ -19,22 +19,30 @@
             }
             mediaIdades = somaIdades/vetorIdades.length;
 
-            for(let i = 0; i < 3; i++){
-                if(vetorIdades[i] > maiorIdade) {
-                    maiorIdade = vetorIdades[i];
+            maiorIdade = Math.max(...vetorIdades);
+
+            for(let i = 0; i < vetorIdades.length; i++) {
+                if(vetorIdades[i] === maiorIdade) {
+                    posicaoMaiorIdade.push(i);
                 }
-               // posicaoMaiorIdade.push(i)
             }
+            alert(`As idades inseridas foram ${vetorIdades}`);
             alert(`A soma de todas as idades é ${somaIdades}, e a média é ${mediaIdades.toFixed(2)}`);
-            alert(`As pessoas com mais  de 25 anos estao localizada(s) no(s) indice(s) (${posicaoPessoasMais25})`);
-            alert(`A maior idade digitada foi ${maiorIdade}`);
-            //alert(`A posicao da maior idade é ${posicaoMaiorIdade}`)
+
+            let posicaoPessoasMais25_Plural_Ou_Singular = posicaoPessoasMais25.length > 1 ? `As pessoas com mais de 25 anos estão localizadas nos indices` : `A pessoa com mais de 25 anos está localizada no indice`;
+            if(posicaoPessoasMais25.length === 0){
+                alert(`Nenhuma das idades inseridas tem mais de 25 anos`);
+            }else{
+                alert(`${posicaoPessoasMais25_Plural_Ou_Singular} (${posicaoPessoasMais25})`);
+            }
+
+            alert(`A maior idade digitada foi ${maiorIdade} e ela apareceu ${posicaoMaiorIdade.length}x,localizada nos indices (${posicaoMaiorIdade})`);
 
             console.log(vetorIdades);
-            console.log(somaIdades);
-            console.log(mediaIdades.toFixed(2));
-            console.log(posicaoPessoasMais25);
-            console.log(maiorIdade);
-            console.log(posicaoMaiorIdade);
+            console.log(`A soma das idades ${somaIdades}`);
+            console.log(`A média das idades ${mediaIdades.toFixed(2)}`);
+            console.log(`Posicao pessoa com mais de 25 anos (${posicaoPessoasMais25})`);
+            console.log(`A maior idade digitada (${maiorIdade})`);
+            console.log(`A posicao da pessoa com maior idade (${posicaoMaiorIdade})`);
             
     }
