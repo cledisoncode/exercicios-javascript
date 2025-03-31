caf8658f8aa854f75da8bd4eea0df4366dfc6773
cledisoncode <200339853+cledisoncode@users.noreply.github.com>
2025-03-30 22:00:51 -0300

    .

---
+++ exteste/outroTeste.js
@@ -0,0 +1,9 @@
+//!pos-incremento
+let x = 10;
+let y = x++
+console.log(x,y);// *  y recebe o valor 10, e então x é incrementado para 11
+
+//?pre-incremento
+let z = --x;// * x é incrementado para 6, e então y recebe o valor 6
+console.log(x,z);
+
